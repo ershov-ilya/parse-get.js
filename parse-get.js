a3a77aaa0c0b31c7de9180786cd5ba3c0b0cef8d
ershov-ilya <ershov.ilya@gmail.com>
2015-09-03 14:45:11 +0300

    get_keys

--- parse-get.js
+++ parse-get.js
@@ -11,7 +11,7 @@
 var GET=(function(){
     var PUBLIC={};
     var config={
-        utm_keys:[],
+        keys:[],
         redirect:false
     };
     var clean_url;
@@ -37,7 +37,7 @@ var GET=(function(){
     };
 
     PUBLIC.parse=function(url){
-        utm_keys = config.get_keys;
+        get_keys = config.keys;
         if(!url || url == '') url = decodeURI(document.location.search);
         if(url.indexOf('?') < 0) return {};
 
@@ -60,8 +60,8 @@ var GET=(function(){
 
         var r,z;
         for (r=0; r<params.length; r++){
-            for (z=0; z<utm_keys.length; z++){
-                if(params[r].indexOf(utm_keys[z]+'=') > -1){
+            for (z=0; z<get_keys.length; z++){
+                if(params[r].indexOf(get_keys[z]+'=') > -1){
                     if(params[r].indexOf('=') > -1) {
                         key = params[r].split('=');
                         GET[key[0]]=key[1];
@@ -71,7 +71,7 @@ var GET=(function(){
 
             //
             key = params[r].split('=');
-            if(utm_keys.indexOf(key[0])==-1) {
+            if(get_keys.indexOf(key[0])==-1) {
                 OTHER.push(params[r]);
             }
         }
@@ -90,7 +90,7 @@ var GET=(function(){
 
     PUBLIC.init=function(options){
         PUBLIC.config(options);
-        PUBLIC.parse();
+        return PUBLIC.parse();
     };
 
     return PUBLIC;
