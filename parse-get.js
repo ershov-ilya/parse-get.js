e75ad37908186201a7a7121b6d615ce95180a04d
ershov-ilya <ershov.ilya@gmail.com>
2015-09-03 14:04:08 +0300

    scaffolds

---
+++ parse-get.js
@@ -0,0 +1,22 @@
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 03.09.2015
+ * Time: 14:58
+ */
+
+var GET=(function(){
+    var PUBLIC={};
+    var config={};
+
+    PUBLIC.config=function(){
+
+    };
+
+    PUBLIC.parse=function(url){
+        
+    };
+})();
